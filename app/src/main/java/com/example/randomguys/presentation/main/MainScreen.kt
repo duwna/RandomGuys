9d58e09cb02f9ce10df93afa7dfee499bdc9a391
Grigorii <leogrisha@mail.ru>
2022-12-25 20:42:04 +0400

    Add FloatingActionButton to open settings

--- app/src/main/java/com/example/randomguys/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/randomguys/presentation/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.randomguys.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,19 +9,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.randomguys.R
 import com.example.randomguys.presentation.Screens
 import com.example.randomguys.presentation.main.composable_items.AnimatedRoulette
 
@@ -31,33 +36,45 @@ fun MainScreen(
 ) {
     val state by viewModel.state.collectAsState()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Center,
-        horizontalAlignment = CenterHorizontally
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
 
-        AnimatedRoulette(
-            items = state.rouletteItems,
-            initialAngle = state.rouletteRotationAngle,
-            onAngleChanged = viewModel::onAngleChanged,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-                .padding(30.dp)
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Center,
+            horizontalAlignment = CenterHorizontally
+        ) {
 
-        Box(modifier = Modifier.height(40.dp)) {
-            state.selectedName?.let {
-                Text(
-                    text = it,
-                    fontSize = 36.sp
-                )
+            AnimatedRoulette(
+                items = state.rouletteItems,
+                initialAngle = state.rouletteRotationAngle,
+                onAngleChanged = viewModel::onAngleChanged,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .padding(30.dp)
+            )
+
+            Box(modifier = Modifier.height(40.dp)) {
+                state.selectedName?.let {
+                    Text(
+                        text = it,
+                        fontSize = 36.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
             }
         }
 
-        Button(onClick = { navController.navigate(Screens.SETTINGS.name) }) {
-            Text(text = "open settings")
+        FloatingActionButton(
+            onClick = { navController.navigate(Screens.SETTINGS.name) },
+            modifier = Modifier
+                .align(BottomEnd)
+                .padding(30.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon_settings),
+                contentDescription = "open settings button"
+            )
         }
     }
 }
--- app/src/main/java/com/example/randomguys/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/randomguys/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.randomguys.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,24 +8,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Color(0xFFD0BCFF),
+    secondary = Color(0xFFCCC2DC),
+    tertiary = Color(0xFFEFB8C8)
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-
-        /* Other default colors to override
+    primary = Color(0xFF6650a4),
+    secondary = Color(0xFF625b71),
+    tertiary = Color(0xFF7D5260),
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -34,15 +28,14 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun RandomGuysTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -52,17 +45,9 @@ fun RandomGuysTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
